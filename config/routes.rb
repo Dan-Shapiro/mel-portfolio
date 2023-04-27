d4869f9325003d8ba7e038cf913d9037c4cfdb87
Dan Shapiro <ds.dan.shapiro@gmail.com>
2023-04-27 11:25:17 -0400

    Add user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
   get 'pages/contact'
   root "pages#home"
   resources :posts
+  
+  devise_for :users
+  devise_scope :user do
+    get '/users', to: 'devise/registration#new'
+    get '/users/password', to: 'devise/passwords#new'
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
